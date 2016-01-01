eab43f8cdb0615d14d82d0cc062e5be6d85344d2
kyawthetsein <kyawthetsein@gmail.com>
2016-01-01 01:27:35 -0500

    Update install-vagrant-1.8.1.sh

--- install-vagrant-1.8.1.sh
+++ install-vagrant-1.8.1.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 
-set -x
-set -e
+apt-get install vagrant 1.8.1
+vagrant plugin install vagrant-aws
+vagrant box add dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
 
-if [ ! -e vagrant ]; then
-  apt-get install vagrant 1.8.1
-fi
-
-#cd vagrant
-#git checkout refs/tags/v1.8.1
-#gem install net-ssh -v '3.0.2'
-#bundle install
-#rake install
-
-if [ ! -e ~/.vagrant.d ]; then
-  vagrant plugin install vagrant-aws
-  vagrant box add dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
-fi
