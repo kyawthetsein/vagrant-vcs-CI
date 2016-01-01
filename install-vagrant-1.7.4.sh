9f874cc7b9094ad4c8f943ad9def6043d3dc0693
kyawthetsein <kyawthetsein@gmail.com>
2016-01-01 05:06:12 -0500

    Update install-vagrant-1.7.4.sh

--- install-vagrant-1.7.4.sh
+++ install-vagrant-1.7.4.sh
@@ -3,5 +3,5 @@
 wget https://releases.hashicorp.com/vagrant/1.7.4/vagrant_1.7.4_x86_64.deb
 sudo dpkg -i vagrant_1.7.4_x86_64.deb
 vagrant plugin install vagrant-aws
-vagrant box add dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
+vagrant box add --force dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
 
