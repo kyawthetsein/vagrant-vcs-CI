619699d062af12eea0752ca320f684e488162870
kyawthetsein <kyawthetsein@gmail.com>
2016-01-01 01:29:33 -0500

    Update install-vagrant-1.8.1.sh

--- install-vagrant-1.8.1.sh
+++ install-vagrant-1.8.1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt-get install vagrant 1.8.1
+sudo apt-get install vagrant 1.8.1
 vagrant plugin install vagrant-aws
 vagrant box add dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
 
