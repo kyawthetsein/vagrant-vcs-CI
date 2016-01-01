6cc543b1b9b28feca4b92dd0f9d1110fd057065e
kyawthetsein <kyawthetsein@gmail.com>
2016-01-01 03:58:59 -0500

    Update and rename install-vagrant-1.8.1.sh to install-vagrant-1.7.4.sh

--- install-vagrant-1.7.4.sh
+++ install-vagrant-1.7.4.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 wget https://releases.hashicorp.com/vagrant/1.7.4/vagrant_1.7.4_x86_64.deb
-dpkg -i vagrant_1.7.4_x86_64.deb
+sudo dpkg -i vagrant_1.7.4_x86_64.deb
 vagrant plugin install vagrant-aws
 vagrant box add dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
 
